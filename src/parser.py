e177e22d4592050d7e72b0696912dd07f8b73673
bockdaa <167830185+bockdaa@users.noreply.github.com>
2025-09-30 18:00:29 -0500

    test

---
+++ src/parser.py
@@ -0,0 +1,20 @@
+from tableloader import parsetable
+
+class parse:
+        def __init__(self,tokens):
+                self.table = parsetable().generate()
+                self.in_stream = tokens
+                self.stack = []
+
+        # Table Driven Algorithm
+        def TDA(self):
+                self.stack.append("$")
+                self.stack.append("PROGRAM")
+                A = self.stack[-1]
+
+
+test_content = [('KEYWORD', 'function'), ('IDENTIFIER', 'main'), ('PUNCTUATION', '('), ('IDENTIFIER', >
+
+
+
+
