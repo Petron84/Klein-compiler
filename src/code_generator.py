029d33ad3953396d8d51b8e4661333de47ac2ce0
bockdaa <167830185+bockdaa@users.noreply.github.com>
2025-11-05 13:24:03 -0600

    started creating code generator

---
+++ src/code_generator.py
@@ -0,0 +1,10 @@
+from scanner import KleinError
+import sys
+
+class Generator():
+    def __init__(self,AST,table):
+        self.AST = AST
+        self.symbol_table = table
+
+    def generate():
+        pass
