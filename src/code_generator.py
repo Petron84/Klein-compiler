749130308efcbe00fc66a35d3eb8c4dbc42d8308
bockdaa <167830185+bockdaa@users.noreply.github.com>
2025-11-20 23:18:44 -0600

    fixed?

--- src/code_generator.py
+++ src/code_generator.py
@@ -241,7 +241,23 @@ class Generator():
                     self.write(f"LD   1, 0(5)", "# Store the return value into memory")
                     self.write(f"SUB  5, 5, 4","# Subtract the offset off of the memory pointer")
                 else:
-                    self.write(f"LDC  1, {child_value}(0)","# Load value into register 1")
+                    if child_type == "IDENTIFIER":
+                        params = self.symbol_table[curr_function].parameters
+                        # Determine which parameter is used. Necessary for offset calculations
+                        # If param 1, then offset = 1. If param 2, then offset = 2
+                        for i, p in enumerate(params[1]):
+                            if p[0] == child_value:
+                                offset = i+1
+                            else:
+                                pass # Error
+                        mem_loc = self.stack_frames[curr_function].address - offset
+                        self.write(f"LDC  3, {mem_loc}(0)", f"# Store the target memory location for the parameter {child_value}")
+                        self.write(f"SUB  4, 3, 5", "# Calculate memory offset. I.E. Target = 1023 and Current = 1020, R4 = 3")
+                        self.write(f"ADD  5, 5, 4", "# Add offset to current memory location.")
+                        self.write(f"LD   1, 0(5)", "# Load the value of parameter from memory into register 1")
+                        self.write(f"SUB  5, 5, 4","# Subtract the offset off of the memory pointer")
+                    else:
+                        self.write(f"LDC  1, {child_value}(0)","# Load value into register 1")
 
                 if exp_value == "not":
                     if child_value == "true":
