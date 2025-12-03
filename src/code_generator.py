fa79ff90b6a51e5d4f30711f94873f5f9ed945ff
bockdaa <167830185+bockdaa@users.noreply.github.com>
2025-12-03 17:42:02 -0600

    testing

--- src/code_generator.py
+++ src/code_generator.py
@@ -127,8 +127,8 @@ class Generator():
                 self.write("ST   1, 0(5)", f"# Store return value of into DMEM")
 
         mem_loc = self.stack_frames['main'].return_add # Retrieve memory address of return value
-        self.write(f"ST  1, 0(5)", f"# Store return value of into DMEM")
         self.write(f"LDC  5, {mem_loc}(0)", f"# Store the memory location of main return value")
+        self.write(f"ST  1, 0(5)", f"# Store return value of into DMEM")
         self.write(f"LD   1, 0(5)","# Load Return Value from DMEM")
         self.write("OUT  1, 0, 0", '# Output value from register 1.')
         self.write('LD   5, 0(0)', '# Reset memory pointer')
